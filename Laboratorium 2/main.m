e8ca2005614c93c17cd8a7962ea024aafb51c2d7
RudyPrzyjaciel <szynks20@gmail.com>
2022-03-22 10:34:31 +0100

    main + regulaFalsi

---
+++ Laboratorium 2/main.m
@@ -0,0 +1,18 @@
+close all;
+clear;
+format long;
+
+f1 = @(x) x + log10(x) - sin(x).^2;
+f2 = @(x) x.^2 + log10(x) - sin(x);
+
+iter = 10;
+
+%% Sieczne
+
+%% Regula Falsi
+root = regula_falsi(f1, 1, 4, iter);
+fprintf('Calculated in: %d iterations using Regula Falsi method\n',iter);
+fprintf('The Root of f1 is : %f \n',root);
+
+root = regula_falsi(f2, 1, 2, iter);
+fprintf('The Root of f2 is : %f \n',root);
---
+++ Laboratorium 2/regula_falsi.m
@@ -0,0 +1,14 @@
+function root = regula_falsi(f,x0, x1, iter)
+    i = 1;
+    while i < iter
+        f0=f(x0); %Calculating the value of function at x0
+        f1=f(x1); %Calculating the value of function at x1
+        y=x1-((x1-x0)/(f1-f0))*f1; %[x0,x1] is the interval of the root
+        f2=f(y);
+        if (f1)*(f2)<1
+            x0=y;  %taking the next interval as[x0,x1] = [y,x1]
+        end
+        i = i + 1;
+    end
+    root = y;
+end
