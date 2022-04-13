dbba95a9e3ee6fb2ccdfe9142613e7de33f3ad2c
SK4MIL <kamil10078@gmail.com>
2022-04-13 14:45:50 +0200

    merging

--- Laboratorium 3/main.m
+++ Laboratorium 3/main.m
@@ -8,7 +8,7 @@ Y = [0.98, 0.93, 0.86, .76, .64];
 a = zeros(1,length(X));
 w = zeros(length(X)-1);
 
-xx = 0:0.5:60;
+xx = 0:1:60;
 yy=spline(X,Y,xx);
 
 for i = 1:length(X)
@@ -48,11 +48,17 @@ f = @(x) ...
     (a(4).*(x-w(4,1)).*(x-w(4,2)).*(x-w(4,3)).*(x-w(4,4))) + ...
     (a(5).*(x-w(5,1)).*(x-w(5,2)).*(x-w(5,3)).*(x-w(5,4)));
 
-t = 0:60;
 figure(1);
-plot(f(t));
 hold on;
-plot(xx,yy, 'black--')
-plot(X,Y, '*r');
-legend(["Wielomian","Spline", "Punkty"]);
+plot(xx,f(xx),xx,yy, 'black--',X, Y,'.r')
+%plot(X,Y, '*r');
+plot( 21, f(21), '.b', 'MarkerSize', 20);
+text(21,f(21),'    \leftarrow f(21) = ');
+legend(["Wielomian","Spline", "Punkty","Zadane X = 21"]);
+% figure(1);
+% plot(xx, f(xx), '--b',  X, Y, '.r', 'MarkerSize', 12);
+% hold on;
+% plot( 21, f(21), '.b', 'MarkerSize', 20);
+% text(21,f(21),'    \leftarrow f(21) = ');
+% legend(["Wielomian", "Punkty", "Zadane X = 21"], 'location', 'southwest');
 
